06e4012cfd6ad2243a5882543c4de5855ead3032
박서영 <emilypark0721@gmail.com>
2025-11-05 02:38:20 +0900

    [feat]:entity 수정

--- src/main/java/backend/stamp/stamp/controller/StampController.java
+++ src/main/java/backend/stamp/stamp/controller/StampController.java
@@ -1,4 +1,21 @@
 package backend.stamp.stamp.controller;
 
+
+import backend.stamp.stamp.service.StampService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/stamps")
 public class StampController {
+
+   // private final StampService stampService;
+
+    //스탬프 등록 -> by 가게 검색
+    //@PostMapping
+
+
 }
