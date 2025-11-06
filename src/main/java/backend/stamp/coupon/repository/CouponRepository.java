01c13beec4f56865dea0b60b68368069af9a22a5
Rossenzii <jangmj80@naver.com>
2025-11-06 15:02:56 +0900

    [feat] qr 로직 생성

---
+++ src/main/java/backend/stamp/coupon/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package backend.stamp.coupon.repository;
+
+import backend.stamp.coupon.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
