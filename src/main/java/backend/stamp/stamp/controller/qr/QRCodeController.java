1f0bba87dbb78d0b8a3fbffd4b9c9c02e6b12d78
Rossenzii <jangmj80@naver.com>
2025-11-06 14:27:32 +0900

    [feat] qrcode base code

---
+++ src/main/java/backend/stamp/stamp/controller/qr/QRCodeController.java
@@ -0,0 +1,9 @@
+package backend.stamp.stamp.controller.qr;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/qr")
+public class QRCodeController {
+}
---
+++ src/main/java/backend/stamp/stamp/service/qr/QRCodeService.java
@@ -0,0 +1,7 @@
+package backend.stamp.stamp.service.qr;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QRCodeService {
+}
